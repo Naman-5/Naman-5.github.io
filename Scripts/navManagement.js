0b2d1b9111d643b3ae698778efbf315f108beacf
Naman Mayer <namanmayer5@gmail.com>
2021-03-21 01:56:33 +0530

    finalised the timeline design
    made the required changes to the scripts and css files to accomodate all possible scenarios (various screen size)

--- Scripts/navManagement.js
+++ Scripts/navManagement.js
@@ -1,7 +1,15 @@
 var navOpen = false;
+var positions = ["gfgPos"];
 function openCloseNav() {
     if(navOpen==false)
     {
+        if(window.innerWidth <= 700)
+        {
+            for(var i = 0; i<positions.length;i++)
+            {
+            document.getElementById(positions[i]).style.zIndex = 0;
+            }
+        }
         document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
         navOpen = true;
@@ -11,5 +19,12 @@ function openCloseNav() {
         document.getElementById("mySidebar").style.width = "0";
         document.getElementById("main").style.marginLeft= "0";
         navOpen = false;
+        if(window.innerWidth <= 700)
+        {
+            for(var i = 0; i<positions.length;i++)
+            {
+            document.getElementById(positions[i]).style.zIndex = 2;
+            }
+        }
     }
   }
