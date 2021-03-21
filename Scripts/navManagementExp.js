5d6fb7c2c640cd49eedad5ffeddfe354e756b307
Naman Mayer <namanmayer5@gmail.com>
2021-03-21 21:19:56 +0530

    fixed navgation for mobile devices
    added seperate script for experience.html page

--- Scripts/navManagementExp.js
+++ Scripts/navManagementExp.js
@@ -3,6 +3,13 @@ var positions = ["gfgPos"];
 function openCloseNav() {
     if(navOpen==false)
     {
+        if(window.innerWidth <= 700)
+        {
+            for(var i = 0; i<positions.length;i++)
+            {
+            document.getElementById(positions[i]).style.zIndex = 0;
+            }
+        }
         document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
         navOpen = true;
@@ -12,5 +19,12 @@ function openCloseNav() {
         document.getElementById("mySidebar").style.width = "0";
         document.getElementById("main").style.marginLeft= "0";
         navOpen = false;
+        if(window.innerWidth <= 700)
+        {
+            for(var i = 0; i<positions.length;i++)
+            {
+            document.getElementById(positions[i]).style.zIndex = 2;
+            }
+        }
     }
   }
